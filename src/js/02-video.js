76035715117de2c60383ba6cde822df7a25c807d
IvanImen <134809500+IvanImen@users.noreply.github.com>
2023-10-26 01:45:37 +0300

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle(
+    currentTime => 
+    {localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime)); }, 1000)
+    
+);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+const parsedCurrentTime = JSON.parse(currentTime);
+
+player.setCurrentTime(parsedCurrentTime.seconds);
